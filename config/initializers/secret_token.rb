98f33b84535256e8c770d5d3f826f227e9c200e6
Ivan Storck <ivanoats@gmail.com>
2013-06-12 17:42:28 -0700

    blog sample with vagrant and chef solo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'cee1642f935829c058fcfffd8d3ba7ad5b55139304de2f03a8dc709d266925a80c1b17ddf024223dcf7ee74aa1c58edfc4208f655dabb00cd3ca8e6830880966'
